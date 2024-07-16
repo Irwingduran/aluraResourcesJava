d14d6b30deb89cefa78e85db58305b98a835fba2
Irwing <86335744+Irwingduran@users.noreply.github.com>
2024-07-15 18:23:20 -0600

    commit

---
+++ Datos y Arrays/01 ArrayList y toString/src/com/aluracurso/screenmatch/modelos/Pelicula.java
@@ -0,0 +1,36 @@
+package com.aluracurso.screenmatch.modelos;
+
+import com.aluracurso.screenmatch.calculos.Clasificacion;
+
+public class Pelicula extends Titulo implements Clasificacion {
+    private String Director;
+
+    public String getDirector() {
+        return Director;
+    }
+
+    public void setDirector(String director) {
+        Director = director;
+    }
+
+    @Override
+    public int getClasificacion() {
+        return (int) (calculaMedia() / 2);
+    }
+
+    @Override
+    public String toString() {
+        return "Pelicula: " + this.getNombre() + "(" + getFechaDeLanzamiento() + ")";
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
